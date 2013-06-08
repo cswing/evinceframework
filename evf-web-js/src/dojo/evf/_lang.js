f8f981276c3625dad0fb2b6a2f2638ac67300cb7
Craig Swing <cswing@gmail.com>
2013-06-07 20:44:26 -0400

    tab formatting

--- evf-web-js/src/dojo/evf/_lang.js
+++ evf-web-js/src/dojo/evf/_lang.js
@@ -15,7 +15,7 @@
  */ 
 define(["dijit/form/_TextBoxMixin", "dijit/form/TextBox"], function(_TextBoxMixin, TextBox) {
 
-    // String enhancements
+	// String enhancements
 	String.prototype.startsWith = function(str) { 
 		return this.slice(0, str.length) == str;
 	};
@@ -31,42 +31,42 @@ define(["dijit/form/_TextBoxMixin", "dijit/form/TextBox"], function(_TextBoxMixi
 	// Array enhancements
 	Array.prototype.clone = function() { return this.slice(0); }    
 
-  /*
-   * http://www.tutorialspoint.com/javascript/array_indexof.htm
-   *
-   * This method is a JavaScript extension to the ECMA-262 standard; as such it may not 
-   * be present in other implementations of the standard. 
-   */
-  if (!Array.prototype.indexOf) {
-    Array.prototype.indexOf = function(elt /*, from*/) {
-      var len = this.length;
+	/*
+	 * http://www.tutorialspoint.com/javascript/array_indexof.htm
+	 *
+	 * This method is a JavaScript extension to the ECMA-262 standard; as such it may not 
+	 * be present in other implementations of the standard. 
+	 */
+	if (!Array.prototype.indexOf) {
+		Array.prototype.indexOf = function(elt /*, from*/) {
+			var len = this.length;
 
-      var from = Number(arguments[1]) || 0;
-      from = (from < 0) ? Math.ceil(from) : Math.floor(from);
-      if (from < 0)
-        from += len;
+			var from = Number(arguments[1]) || 0;
+			from = (from < 0) ? Math.ceil(from) : Math.floor(from);
+			if (from < 0)
+				from += len;
 
-      for (; from < len; from++) {
-        if (from in this &&
-            this[from] === elt)
-          return from;
-      }
-      return -1;
-    };
-  }
-  
-    /*
-     * Modify Dojo's default behavior regarding the HTML5 placeHolder - if focused and 
-     * still empty, then the placeholder should still be shown.
-     */
-    TextBox.prototype._updatePlaceHolder = function(){
-        if(this._phspan){
-            this._phspan.style.display=(this.placeHolder&&!this.textbox.value)?"":"none";
-        }
-    };
-    TextBox.prototype._processInput = function(){
-        _TextBoxMixin.prototype._processInput.apply(this, arguments);
-        this._updatePlaceHolder();
-    };
+			for (; from < len; from++) {
+			if (from in this &&
+				this[from] === elt)
+				return from;
+			}
+			return -1;
+		};
+	}
+
+	/*
+	 * Modify Dojo's default behavior regarding the HTML5 placeHolder - if focused and 
+	 * still empty, then the placeholder should still be shown.
+	 */
+	TextBox.prototype._updatePlaceHolder = function(){
+		if(this._phspan){
+			this._phspan.style.display=(this.placeHolder&&!this.textbox.value)?"":"none";
+		}
+	};
+	TextBox.prototype._processInput = function(){
+		_TextBoxMixin.prototype._processInput.apply(this, arguments);
+		this._updatePlaceHolder();
+	};
 
 });
