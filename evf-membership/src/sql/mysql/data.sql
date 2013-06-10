75559b5bab18cef7f127b860d6aa8a7c414e4dbd
Craig Swing <cswing@gmail.com>
2013-06-10 18:57:33 -0400

    Add default membership rights

---
+++ evf-membership/src/sql/mysql/data.sql
@@ -0,0 +1,4 @@
+
+-- Default membership rights
+insert into evf_membership_right(oid, token, description) values(uuid(), 'evf.membership.viewProfile', '');
+insert into evf_membership_right(oid, token, description) values(uuid(), 'evf.membership.signOut', '');
